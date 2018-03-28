00cd11ddc588b7694a0d3675c0c1ed0666da359c
enextus <enextus@gmail.com>
2018-03-28 19:03:31 +0300

    start db new

--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -28,14 +28,7 @@ class ReportWorker
           Button.all
         end
 
-        # User.all.max_by { |user| user.created_at }
-
         User.all.each do |user|
-
-        # User.all.each_with_index do |user, index|
-        # return if index > 0
-
-          binding.pry
 
           bot.api.send_message(chat_id: user.chat_id, text: user.first_name.to_s + getting_msg['welcome_msg'].to_s)
           bot.api.send_message(chat_id: user.chat_id, text: getting_msg['desc_msg'])
