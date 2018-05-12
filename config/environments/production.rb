c1e38793cca3955b09cf4466e48b2cbec0e20d51
enextus <enextus@gmail.com>
2018-05-12 15:18:29 +0300

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  routes.default_url_options = {host: 'enextus.com', protocol: 'https'} # https is necessary!
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -75,6 +78,9 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
