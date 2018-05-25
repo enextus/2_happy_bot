6c9f24fc971018272a586e50a8c11a5169c4df23
enextus <enextus@gmail.com>
2018-05-25 11:02:24 +0300

    1 times at day 12.00

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,61 +1,12 @@
-# env :GEM_PATH, ENV['GEM_PATH']
 job_type :sidekiq, "cd :path && RAILS_ENV=:environment /usr/local/bin/bundle exec /usr/local/bin/sidekiq-client :task :output"
-
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
 set :chronic_options, hours24: true
-
-# adds ">> cron.log 2> error.log" to all commands
-
 set :output, error: 'error.log', standard: 'cron.log'
-
 set :environment, :development
 
-# By default this would run the job every day at 3am
-# every 1.day, at: '3:00' do
-  # runner "HardWorker.perform_async "like a dog", 3"
-# end
-
-every 1.day, at: '09:00' do
-  runner "ReportingWorker.perform_async '24.05.2018 reporting worker', 5 ", :output => 'cron.log'
-end
-
-every 1.day, at: '13:00' do
-  runner "ReportingWorker.perform_async '24.05.2018 reporting worker', 5 ", :output => 'cron.log'
-end
-
-every 1.day, at: '19:00' do
-  runner "ReportingWorker.perform_async '24.05.2018 reporting worker', 5 ", :output => 'cron.log'
-end
-
-every 30.minute do
-  runner "ReportingWorker.perform_async '24.05.2018 reporting worker', 3 ", :output => 'cron.log'
+every 1.day, at: '12:00' do
+  runner "ReportingWorker.perform_async '25.05.2018 reporting worker', 5 ", :output => 'cron.log'
 end
 
 # every 10.minutes do
-  # sidekiq 'push ReportingWorker'
-# #nd
-
-# every :hour do # Many shortcuts available: :hour, :day, :month, :year, :reboot
-#  runner "SomeModel.ladeeda"
-#  runner "HardWorker.perform_async "like a dog", 3"
+#   sidekiq 'push ReportingWorker'
 # end
