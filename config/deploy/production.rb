e9157d1b41f1edb44e2ac0a3979d42da9805498f
enextus <enextus@gmail.com>
2019-02-16 22:49:50 +0300

    configs2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'example.com', user: 'deploy', roles: %w{app db web}
+server '178.62.220.123', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
