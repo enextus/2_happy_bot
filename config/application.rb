c3d0e16d6bcee69f3c042b4817678aa355117d99
enextus <enextus@gmail.com>
2019-03-22 13:44:40 +0300

    gemfile

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'telegram/bot'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+# Dotenv::Railtie.load
 
 HOSTNAME = ENV['HOSTNAME']
 
