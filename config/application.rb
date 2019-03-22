365fb7a84b27814ebc36937ddd1159128cf18645
enextus <enextus@gmail.com>
2019-03-22 13:28:42 +0300

    dotenv 2

--- config/application.rb
+++ config/application.rb
@@ -4,11 +4,11 @@ require 'telegram/bot'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-
-Dotenv::Railtie.load
-
-HOSTNAME = ENV['HOSTNAME']
+# Bundler.require(*Rails.groups)
+#
+# Dotenv::Railtie.load
+#
+# HOSTNAME = ENV['HOSTNAME']
 
 module HappyBot
   class Application < Rails::Application
