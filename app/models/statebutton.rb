0aa2b66d1a7b9c31e1d1feecf589c67edd6be2cc
enextus <enextus@gmail.com>
2019-06-04 10:06:44 +0300

    some rev

--- app/models/statebutton.rb
+++ app/models/statebutton.rb
@@ -3,5 +3,7 @@ class Statebutton < ApplicationRecord
 
   scope :data_of_chat_id_1, -> { where(chat_id: 1) }
   scope :data_of_chat_id_2, -> { where(chat_id: 2) }
+  scope :data_of_chat_id_3, -> { where(chat_id: 3..100) }
+
   scope :recent, -> { order(created_at: :desc) }
 end
