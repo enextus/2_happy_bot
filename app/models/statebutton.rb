39ddf5f47a9fc5b4832caa35928a3e5b26f68a0c
enextus <enextus@gmail.com>
2019-06-11 12:59:24 +0300

    statebutton scope

--- app/models/statebutton.rb
+++ app/models/statebutton.rb
@@ -1,8 +1,6 @@
 class Statebutton < ApplicationRecord
   belongs_to :chat, class_name: 'Chat'
 
-
-  scope :data_of_chat_id, -> (id) { where(chat_id: id) }
-
+  scope :data_by_chat_id, -> (id) { where(chat_id: id) }
   scope :recent, -> { order(created_at: :desc) }
 end
