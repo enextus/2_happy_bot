07ffee764a7262b23a98a748e80b64fae09b6f56
enextus <enextus@gmail.com>
2019-07-09 22:27:25 +0300

    routes rev

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'users#getting_user_message'
+  # get '/', to: 'users#getting_user_message'
+  # post '/', to: 'users#getting_user_message'
 
-  post '/', to: 'users#getting_user_message'
+  match '/', to: 'users#getting_user_message', via: %i[get post]
 
   mount Sidekiq::Web => '/sidekiq'
 end
