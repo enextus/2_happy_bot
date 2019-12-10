c741cd18cb89249fa99afe304a6231f1edcfcfae
enextus <enextus@gmail.com>
2019-12-10 17:37:35 +0100

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   TOKEN = ENV['TOKEN']
 
   # Delay time time until last write to the db
-  DELAY = 120
+  DELAY = 100
 
   # check interval after last saving by each user in the DB
   def timer(login)
