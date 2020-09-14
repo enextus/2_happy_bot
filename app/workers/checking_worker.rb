e5bff895e4701d0b794817cdc353e35051fc349b
enextus <enextus@gmail.com>
2020-09-14 10:11:04 +0200

    perform

--- app/workers/checking_worker.rb
+++ app/workers/checking_worker.rb
@@ -9,14 +9,14 @@ class CheckingWorker
   include Sidekiq::Worker
 
   def perform(run_time, end_time, user_chat_id, replay_id)
-    sleep 7236
 
-    # Do something
+    # CheckingWorker.perform_async(run_time, end_time, user_chat_id, replay_id)
+
+    sleep 3600 * 3
 
     state_button = Statebutton.where(created_at: run_time..end_time, chat_id: user_chat_id)
-    return unless state_button.size.positive?
 
-    # Do something
+    return unless state_button.size.positive?
 
     Request.find_by(id: replay_id)&.update(response: true)
   end
