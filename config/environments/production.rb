2f0116340598924ed7bb8528a75f3306f7412784
enextus <enextus@gmail.com>
2021-06-14 14:14:44 +0200

    upd3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.enabled = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -25,9 +27,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = Uglifier.new(harmony: true, compress: { unused: false })
   config.assets.js_compressor = Uglifier.new(harmony: true, compress: { unused: false })
 
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
