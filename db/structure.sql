7ef43f08ccd12209cf75807e12b8e487eeb712d9
enextus <enextus@gmail.com>
2022-04-15 17:51:27 +0200

    15.04.2022 001

--- db/structure.sql
+++ db/structure.sql
@@ -37,7 +37,6 @@ CREATE TABLE public.buttons (
     updated_at timestamp without time zone NOT NULL
 );
 
-
 --
 -- Name: buttons_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
@@ -49,13 +48,11 @@ CREATE SEQUENCE public.buttons_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
 --
 -- Name: buttons_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.buttons_id_seq OWNED BY public.buttons.id;
-
 
 --
 -- Name: chats; Type: TABLE; Schema: public; Owner: -
@@ -69,7 +66,6 @@ CREATE TABLE public.chats (
     user_id bigint
 );
 
-
 --
 -- Name: chats_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
@@ -81,13 +77,11 @@ CREATE SEQUENCE public.chats_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
 --
 -- Name: chats_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.chats_id_seq OWNED BY public.chats.id;
-
 
 --
 -- Name: messages; Type: TABLE; Schema: public; Owner: -
@@ -101,7 +95,6 @@ CREATE TABLE public.messages (
     updated_at timestamp without time zone NOT NULL
 );
 
-
 --
 -- Name: messages_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
@@ -113,13 +106,11 @@ CREATE SEQUENCE public.messages_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
 --
 -- Name: messages_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.messages_id_seq OWNED BY public.messages.id;
-
 
 --
 -- Name: requests; Type: TABLE; Schema: public; Owner: -
@@ -133,7 +124,6 @@ CREATE TABLE public.requests (
     chat_id bigint
 );
 
-
 --
 -- Name: requests_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
@@ -145,13 +135,11 @@ CREATE SEQUENCE public.requests_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
 --
 -- Name: requests_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.requests_id_seq OWNED BY public.requests.id;
-
 
 --
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
@@ -160,7 +148,6 @@ ALTER SEQUENCE public.requests_id_seq OWNED BY public.requests.id;
 CREATE TABLE public.schema_migrations (
     version character varying NOT NULL
 );
-
 
 --
 -- Name: statebuttons; Type: TABLE; Schema: public; Owner: -
@@ -176,7 +163,6 @@ CREATE TABLE public.statebuttons (
     chat_id bigint
 );
 
-
 --
 -- Name: statebuttons_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
@@ -188,13 +174,11 @@ CREATE SEQUENCE public.statebuttons_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
 --
 -- Name: statebuttons_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.statebuttons_id_seq OWNED BY public.statebuttons.id;
-
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: -
@@ -207,7 +191,6 @@ CREATE TABLE public.users (
     updated_at timestamp without time zone NOT NULL
 );
 
-
 --
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
@@ -219,13 +202,11 @@ CREATE SEQUENCE public.users_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
 --
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
 
 --
 -- Name: buttons id; Type: DEFAULT; Schema: public; Owner: -
@@ -233,13 +214,11 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 ALTER TABLE ONLY public.buttons ALTER COLUMN id SET DEFAULT nextval('public.buttons_id_seq'::regclass);
 
-
 --
 -- Name: chats id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.chats ALTER COLUMN id SET DEFAULT nextval('public.chats_id_seq'::regclass);
-
 
 --
 -- Name: messages id; Type: DEFAULT; Schema: public; Owner: -
@@ -247,13 +226,11 @@ ALTER TABLE ONLY public.chats ALTER COLUMN id SET DEFAULT nextval('public.chats_
 
 ALTER TABLE ONLY public.messages ALTER COLUMN id SET DEFAULT nextval('public.messages_id_seq'::regclass);
 
-
 --
 -- Name: requests id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.requests ALTER COLUMN id SET DEFAULT nextval('public.requests_id_seq'::regclass);
-
 
 --
 -- Name: statebuttons id; Type: DEFAULT; Schema: public; Owner: -
@@ -261,13 +238,11 @@ ALTER TABLE ONLY public.requests ALTER COLUMN id SET DEFAULT nextval('public.req
 
 ALTER TABLE ONLY public.statebuttons ALTER COLUMN id SET DEFAULT nextval('public.statebuttons_id_seq'::regclass);
 
-
 --
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
 
 --
 -- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
@@ -276,14 +251,12 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 ALTER TABLE ONLY public.ar_internal_metadata
     ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
 
-
 --
 -- Name: buttons buttons_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.buttons
     ADD CONSTRAINT buttons_pkey PRIMARY KEY (id);
-
 
 --
 -- Name: chats chats_pkey; Type: CONSTRAINT; Schema: public; Owner: -
@@ -292,14 +265,12 @@ ALTER TABLE ONLY public.buttons
 ALTER TABLE ONLY public.chats
     ADD CONSTRAINT chats_pkey PRIMARY KEY (id);
 
-
 --
 -- Name: messages messages_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.messages
     ADD CONSTRAINT messages_pkey PRIMARY KEY (id);
-
 
 --
 -- Name: requests requests_pkey; Type: CONSTRAINT; Schema: public; Owner: -
@@ -308,14 +279,12 @@ ALTER TABLE ONLY public.messages
 ALTER TABLE ONLY public.requests
     ADD CONSTRAINT requests_pkey PRIMARY KEY (id);
 
-
 --
 -- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.schema_migrations
     ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
-
 
 --
 -- Name: statebuttons statebuttons_pkey; Type: CONSTRAINT; Schema: public; Owner: -
@@ -324,7 +293,6 @@ ALTER TABLE ONLY public.schema_migrations
 ALTER TABLE ONLY public.statebuttons
     ADD CONSTRAINT statebuttons_pkey PRIMARY KEY (id);
 
-
 --
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
@@ -332,13 +300,11 @@ ALTER TABLE ONLY public.statebuttons
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
-
 --
 -- Name: index_chats_on_user_id; Type: INDEX; Schema: public; Owner: -
 --
 
 CREATE INDEX index_chats_on_user_id ON public.chats USING btree (user_id);
-
 
 --
 -- Name: index_requests_on_chat_id; Type: INDEX; Schema: public; Owner: -
@@ -346,13 +312,11 @@ CREATE INDEX index_chats_on_user_id ON public.chats USING btree (user_id);
 
 CREATE INDEX index_requests_on_chat_id ON public.requests USING btree (chat_id);
 
-
 --
 -- Name: index_statebuttons_on_chat_id; Type: INDEX; Schema: public; Owner: -
 --
 
 CREATE INDEX index_statebuttons_on_chat_id ON public.statebuttons USING btree (chat_id);
-
 
 --
 -- Name: requests fk_rails_159de262dc; Type: FK CONSTRAINT; Schema: public; Owner: -
@@ -361,7 +325,6 @@ CREATE INDEX index_statebuttons_on_chat_id ON public.statebuttons USING btree (c
 ALTER TABLE ONLY public.requests
     ADD CONSTRAINT fk_rails_159de262dc FOREIGN KEY (chat_id) REFERENCES public.chats(id);
 
-
 --
 -- Name: statebuttons fk_rails_676a3a6fd3; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
@@ -369,14 +332,12 @@ ALTER TABLE ONLY public.requests
 ALTER TABLE ONLY public.statebuttons
     ADD CONSTRAINT fk_rails_676a3a6fd3 FOREIGN KEY (chat_id) REFERENCES public.chats(id);
 
-
 --
 -- Name: chats fk_rails_e555f43151; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.chats
     ADD CONSTRAINT fk_rails_e555f43151 FOREIGN KEY (user_id) REFERENCES public.users(id);
-
 
 --
 -- PostgreSQL database dump complete
@@ -391,5 +352,3 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20180415163930'),
 ('20180415164435'),
 ('20180417063537');
-
-
